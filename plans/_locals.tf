012a098d65e0e6a2a9a94de4fea4ab93847e2b9f
chris <chris@trivialsec.com>
2023-04-25 22:52:44 +1000

    fix: IAM

--- plans/_locals.tf
+++ plans/_locals.tf
@@ -1,19 +1,19 @@
 locals {
-    aws_master_account_id = 984310022655
-    aws_default_region    = "ap-southeast-2"
-    python_version        = "python3.10"
-    source_file           = "${lower(var.app_env)}-${var.app_name}.zip"
-    function_name         = "${lower(var.app_env)}-feed-processor-dataplane"
-    tags                  = {
-        ProjectName = "early-warning-service"
-        ProjectLeadEmail = "chris@trivialsec.com"
-        CostCenter = var.app_env != "Prod" ? "randd" : "opex"
-        SecurityTags = "public-data"
-        AutomationTool = "Terraform"
-        LUMIGO_TAG       = var.app_env == "Prod" ? "Production" : "Development"
-    }
-    timeout               = 900
-    memory_size           = 256
-    retention_in_days     = var.app_env == "Prod" ? 30 : 7
-    enable_dynatrace      = false # var.app_env == "Prod"
+  aws_master_account_id = 984310022655
+  aws_default_region    = "ap-southeast-2"
+  python_version        = "python3.10"
+  source_file           = "${lower(var.app_env)}-${var.app_name}.zip"
+  function_name         = "${lower(var.app_env)}-feed-processor-dataplane"
+  tags = {
+    ProjectName      = "early-warning-service"
+    ProjectLeadEmail = "chris@trivialsec.com"
+    CostCenter       = var.app_env != "Prod" ? "randd" : "opex"
+    SecurityTags     = "public-data"
+    AutomationTool   = "Terraform"
+    LUMIGO_TAG       = var.app_env == "Prod" ? "Production" : "Development"
+  }
+  timeout           = 900
+  memory_size       = 256
+  retention_in_days = var.app_env == "Prod" ? 30 : 7
+  enable_dynatrace  = false # var.app_env == "Prod"
 }
